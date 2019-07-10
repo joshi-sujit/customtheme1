86526013a58e0003e21b5ad6192e353d386ecfa7
suzit <its.suzit@gmail.com>
2019-07-10 14:25:54 +1000

    Adding Custom Product Tab in product edit page including the save function for custom field

--- wp-content/plugins/woo-add-prod-section/woo-add-prod-section.php
+++ wp-content/plugins/woo-add-prod-section/woo-add-prod-section.php
@@ -24,24 +24,55 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
         {
             public function __construct()
             {
-                add_filter('woocommerce_product_data_tabs', array($this, 'add_product_tabs'));
-                add_action('woocommerce_product_data_panels', array($this, 'display_product_tabs'));
+                add_filter('woocommerce_product_data_tabs', array($this, 'add_product_tabs'), 50);
+                add_action('woocommerce_product_data_panels', array($this, 'display_spec_tabs'), 50);
+                add_action('woocommerce_process_product_meta', array($this, 'save_data_spec_tabs'), 51);
             }
+
+            /**
+             * Add a product data tabs in the Product edit page
+             */
 
             public function add_product_tabs($product_tabs)
             {
-                $product_tabs['Downloads'] = array(
-                    'label'    => __('Downloads', 'woo_add_prod_section'),
-                    'target'   => 'downloads_product_data',
-                    'priority' => 20,
+                $product_tabs['spec_downloads'] = array(
+                    'label'    => __('Spec Downloads', 'woo_add_prod_section'),
+                    'target'   => 'display_spec_tabs',
+                    'priority' => 10001,
+                    'class'     => array()
                 );
                 return $product_tabs;
             }
 
-            public function display_product_tabs()
+            /**
+             * Add a display setting section for the tab created in the add_product_tabs
+             * Notice the div id echoed on top which is equivalent to target set on add_product_tabs
+             */
+
+            public function display_spec_tabs()
             {
-                echo "Add Specification";
-                echo "Add User Manuals";
+                echo '<div id="display_spec_tabs" class="panel woocommerce_options_panel">';
+                $specification_link = array(
+                    'id'    => 'specification_link_id',
+                    'name' =>  __('specification_link', 'woo_add_prod_section'),
+                    'label'     => __('Spec Download Link', 'woo_add_prod_section'),
+                    'desc_tip' => __('Add a link to the product specification document', 'woo_add_prod_section')
+
+                );
+                woocommerce_wp_text_input($specification_link);
+                echo '</div>';
+            }
+
+
+            /**
+             * Saving the Spec Link data using a update_post_meta
+             * @params $post_id
+             */
+            public function save_data_spec_tabs($post_id)
+            {
+                if (isset($_POST['specification_link'])) {
+                    update_post_meta($post_id, 'specification_link_id', $_POST['specification_link']);
+                }
             }
         }
 
